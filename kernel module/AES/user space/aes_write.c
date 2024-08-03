eb2c562673cf68fc86b7afd615d03dc191fcf695
Gino Fang <43226625+CHFang731@users.noreply.github.com>
2024-08-03 15:35:28 +0800

    Add files via upload

---
+++ kernel module/AES/user space/aes_write.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+int main(int argc, char *argv[])
+{
+    int IP_fd;
+    unsigned char data = 0x55;
+    IP_fd = open("/dev/aes",O_RDWR);
+    if(IP_fd == -1){
+	perror("cannot open the device aes!!\n");
+	exit(1);
+    }
+    if(write(IP_fd, &data, sizeof(char))){
+	perror("write() error!\n");
+	close(IP_fd);
+	exit(1);
+    }
+	close(IP_fd);
+	return 0;
+}
+
